58d13c1b2a6200214c3dbf50a64aad6c95b96e65
DrianH <drianhoxha02@gmail.com>
2023-10-09 11:10:22 +0200

    Changed main.js port, added docker-compose and separate Dockerfiles

--- static/main.js
+++ static/main.js
@@ -23238,7 +23238,7 @@ globalThis.$localize = Object.assign(globalThis.$localize || {}, { locale: "en-U
       // The list of file replacements can be found in `angular.json`.
       const environment = {
         production: false,
-        apiBaseUrl: '/api/',
+        apiBaseUrl: 'http://localhost:8080/',
         apiVersion: '2',
         appTitle: 'Paperless-ngx',
         version: 'DEVELOPMENT',
